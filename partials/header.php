5907bcd25aff0e1376a5fae4b8054d844a0e3f7f
Caê Penna <caepenna@gmail.com>
2019-05-14 18:55:25 -0300

    Add columns stylesheet (commented, may not be needed)

--- partials/header.php
+++ partials/header.php
@@ -16,8 +16,11 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="stylesheet" href="https://use.typekit.net/zgj0rir.css">
     <?php wp_head(); ?>
   </head>
   <body>
     <header>
+      <h1>header</h1>
+      <h4>header</h4>
     </header>
