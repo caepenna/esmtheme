119eae13f67059ccc131333ff29039cd4e3c80cd
Caê Penna <caepenna@gmail.com>
2019-05-14 22:08:11 -0300

    Basic style for section-follw and esm-footer

--- partials/footer.php
+++ partials/footer.php
@@ -11,27 +11,27 @@
     $baseurl = get_template_directory_uri();
 ?>
 
-    <footer class="esm-footer">
 
-      <section>
+      <section class="section-follow">
         <div class="board">
-          <h2>Acompanhe a Frente</h2>
-          <p>Participe da nossa lista de e-mails e fique por dentro de todas as novidades sobre a Frente, projetos e ações parceiros.</p>
-          <form class="" action="index.html" method="post">
+          <h2 class="title -uppercase">Acompanhe a Frente</h2>
+          <p class="subtitle">Participe da nossa lista de e-mails e fique por dentro de todas as novidades sobre a Frente, projetos e ações parceiros.</p>
+          <form class="form" action="index.html" method="post">
             <input type="text" name="name" placeholder="seu nome">
             <input type="text" name="email" placeholder="seu e-mail">
+            <br>
             <button type="submit">Cadastrar</button>
           </form>
         </div>
       </section>
 
-      <section>
+      <footer class="esm-footer">
         <div class="left">
           <img src="" alt="">
         </div>
         <div class="right">
-          <h3>Encontre a Frente nas mídias:</h3>
-          <nav>
+          <h4>Encontre a Frente nas mídias:</h4>
+          <nav class="medias">
             <a href="#">Facebook</a>
             <a href="#">Twitter</a>
             <a href="#">Instagram</a>
@@ -39,10 +39,10 @@
             <a href="#">Flickr</a>
           </nav>
         </div>
-      </section>
-
     </footer>
 
     <?php wp_footer(); ?>
+
   </body>
+
 </html>
