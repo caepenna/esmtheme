5f4920340d17b532236fe7fd71386738c9d56a36
Vítor Marques de Faria <vtrmrx@gmail.com>
2019-05-22 11:50:40 -0300

    cria página de postagem e componente de posts relacionados

--- single.php
+++ single.php
@@ -14,9 +14,29 @@
 
 <?php while ( have_posts() ) : the_post(); ?>
 
-  <main>
+    <?php
+        $postcat = get_the_category( $post->ID );
+        if ( ! empty( $postcat ) ):
+          $area_slug = $postcat[0]->slug;
+          $area_name = $postcat[0]->name;
+          $term_id = $postcat[0]->term_id;
+          $taxonomy = $postcat[0]->taxonomy;
+        endif;
+    ?>
 
-  </main>
+    <main>
+        <div>
+            <span><?php echo $area_name; ?></span>
+        </div>
+        <h1><?php the_title(); ?></h1>
+        <div>
+            <?php echo wp_get_attachment_image(get_post_thumbnail_id(), 'large', '', array('class' => '')); ?>
+        </div>
+        <div>
+            <?php the_content(); ?>
+        </div>
+    </main>
+		<?php get_template_part( 'partials/related-posts' ); ?>
 
 <?php endwhile; ?>
 
