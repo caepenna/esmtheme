3adced8deaca395cabaff53918299ca55da6f8ab
Vítor Marques de Faria <vtrmrx@gmail.com>
2019-05-22 13:17:05 -0300

    cria alternativa mais simples pra filtros, criando links para páginas de categorias em vez de filtrar e carregar mais posts via ajax.

--- partials/filters.php
+++ partials/filters.php
@@ -6,8 +6,10 @@
  */
 ?>
 
-<div>
-  <a class="filter filter_posts active" data-post-type="post" data-post-category="">Todas</a>
+<ul>
+  <li>
+    <a class="filter filter_posts active" data-post-type="post" data-post-category="">Todas</a>
+  </li>
 
   <?php
     $categories = get_terms( array(
@@ -16,10 +18,14 @@
     ) );
     foreach($categories as $term) {
       if( 0 != $term->count ): ?>
+        <li>
+          <a href="<?php echo get_category_link( $term->term_taxonomy_id ); ?>"><?php echo $term->name; ?></a>
 
-        <a class="filter filter_posts" data-post-type="post" data-category="<?php echo $term->slug; ?>"><?php echo $term->name; ?></a>
+          <!-- A alternativa a seguir é caso nós usemos ajax pra carregar a categoria dentro da página -->
+          <!--<a class="filter filter_posts" data-post-type="post" data-category="<?php //echo $term->slug; ?>"><?php //echo $term->name; ?></a>-->
+        </li>
 
       <?php endif;
     }
   ?>
-</div>
+</ul>
