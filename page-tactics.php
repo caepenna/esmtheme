33256f4db00d40d49d368bc5b06673b0fa5225e2
Vítor Marques de Faria <vtrmrx@gmail.com>
2019-05-26 23:17:51 -0300

    estiliza página de estratégias

--- page-tactics.php
+++ page-tactics.php
@@ -16,37 +16,40 @@
       <div class="board">
         <h3 class="title"><?php the_title(); ?></h3>
         <?php the_content(); ?>
+        <nav>
+          <a href="#">Compartilhar</a>
+          <a href="#">Baixar</a>
+          <a href="#">Imprimir</a>
+        </nav>
       </div>
-      <nav>
-        <a href="#">Compartilhar</a>
-        <a href="#">Baixar</a>
-        <a href="#">Imprimir</a>
-      </nav>
     </section>
 
     <?php if( have_rows('estrategias') ): ?>
-    	<section class="estrategias">
+    	<section class="section-tactics">
       	<?php while( have_rows('estrategias') ): the_row();
       		$titulo = get_sub_field('titulo');
       		$texto = get_sub_field('texto');
       		$imagem = get_sub_field('imagem');
       	?>
-      		<article class="estrategia">
+      		<article class="tactic">
+            <div class="info">
+              <?php if( $titulo ): ?>
+        				<h2><?php echo $titulo ?></h2>
+        			<?php endif; ?>
 
-      			<?php if( $titulo ): ?>
-      				<h2><?php echo $titulo ?></h2>
-      			<?php endif; ?>
+              <?php if( $texto ): ?>
+        				<p><?php echo $texto ?></p>
+        			<?php endif; ?>
+            </div>
+            <div class="illustration">
+              <!-- Se precisar da url (pra usar como background-image, por exemplo) -->
+              <?php //echo wp_get_attachment_url($imagem); ?>
 
-            <?php if( $texto ): ?>
-      				<p><?php echo $texto ?></p>
-      			<?php endif; ?>
-
-            <!-- Se precisar da url (pra usar como background-image, por exemplo) -->
-            <?php echo wp_get_attachment_url($imagem); ?>
-
-            <!-- Se for usar um elemento img. Com essa função, o Wordpress gera uma imagem responsiva usando o atributo srcset -->
-            <?php echo wp_get_attachment_image($imagem, 'medium_large'); ?>
-
+              <!-- Se for usar um elemento img. Com essa função, o Wordpress gera uma imagem responsiva usando o atributo srcset -->
+              <div class="image">
+                <?php echo wp_get_attachment_image($imagem, 'medium_large'); ?>
+              </div>
+            </div>
       		</article>
       	<?php endwhile; ?>
       </section>
