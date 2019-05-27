e2e0240f57d5efbb3950eff871afd8bed4b4f14e
Vítor Marques de Faria <vtrmrx@gmail.com>
2019-05-26 21:56:32 -0300

    lista projetos na página de projetos

--- page-projects.php
+++ page-projects.php
@@ -8,6 +8,13 @@
   get_template_part( 'partials/header' );
 ?>
 
+<?php
+  $projetos = get_posts(array(
+  	'numberposts'	=>     -1,
+  	'post_type'		=>     'projeto'
+  ));
+?>
+
 <?php while ( have_posts() ) : the_post(); ?>
 
   <main>
@@ -18,24 +25,72 @@
 
     <section class="section-projects">
       <div class="list">
+
+        <!-- Estado -->
         <select>
-          <option value="">Outro Preto / MG</option>
-          <option value="">Campo Grande / MS</option>
+          <option value="">MG</option>
+          <option value="">MS</option>
         </select>
+
+        <!-- Município -->
+        <select>
+          <option value="">Outro Preto</option>
+          <option value="">Campo Grande</option>
+        </select>
+
+        <!-- Tipo de projeto, "Tipo" -->
+        <select>
+          <option value="">Escola sem Partido</option>
+          <option value="">Antigênero</option>
+          <option value="">Antipornografia</option>
+        </select>
+
+        <!-- Status do projeto, "Status" -->
+        <!-- <select>
+          <option value="">Em Vigor</option>
+          <option value="">Sancionado</option>
+          <option value="">Retirado</option>
+        </select> -->
+
         <ul>
-          <li>
-            <h5>Nome do projeto</h5>
-            <p><em>Responsável:</em> Nome do responsável</p>
-            <p><em>Telefone:</em> 61 99298 1313</p>
-            <p><em>Email:</em> contato@gmail.com</p>
-          </li>
-          <li>
-            <h5>Nome do projeto</h5>
-            <p><em>Responsável:</em> Nome do responsável</p>
-            <p><em>Telefone:</em> 61 99298 1313</p>
-            <p><em>Email:</em> contato@gmail.com</p>
-          </li>
+          <!-- Loop que constrói lista de projetos -->
+          <?php  $i = 1; foreach( $projetos as $post ): setup_postdata( $post ); ?>
+            <?php
+              //vars
+              $titulo = get_the_title();
+              $responsavel = get_field('responsavel');
+              $telefone = get_field('telefone');
+              $email = get_field('email');
+              $municipio = get_field('municipio');
+              $coordenadas = get_field('coordenadas');
+              $status = get_field('status');
+              $id = $post->post_name;
+            ?>
+            <li>
+
+              <?php if( $titulo ): ?>
+                <h5><?php echo $titulo ?></h5>
+              <?php endif; ?>
+
+              <?php if( $responsavel ): ?>
+                <p><em>Responsável: </em><?php echo $responsavel ?></p>
+              <?php endif; ?>
+
+              <?php if( $telefone ): ?>
+                <p><em>Telefone: </em><?php echo $telefone ?></p>
+              <?php endif; ?>
+
+              <?php if( $email ): ?>
+                <p><em>Email: </em><?php echo $email ?></p>
+              <?php endif; ?>
+
+
+            </li>
+
+          <?php $i++; endforeach; wp_reset_postdata(); ?>
+
         </ul>
+
       </div>
       <div class="map">
         <div id="map"></div>
