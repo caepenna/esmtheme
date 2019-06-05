1db2228a8ef36088adf9a822158c52f1e451adca
Vítor Marques de Faria <vtrmrx@gmail.com>
2019-06-04 22:07:28 -0300

    carrega links, títulos e thumbs dos 10 últimos vídeos do canal e link do canal com api do youtube

--- partials/gallery-videos.php
+++ partials/gallery-videos.php
@@ -1,79 +1,43 @@
 <?php
 /**
- * Galeria de notícias
+ * Galeria de vídeos
  *
  * @package ESM
  */
 ?>
 
 <div class="gallery-videos">
-
     <div id="ajaxContainer">
 
-      <?php for ($i = 1; $i <= 3; $i++): ?>
+      <?php
 
-        <a href="" class="video">
-          <div class="cover">
-            <div id="player_<?php echo $i; ?>" class="thumbnail"></div>
-            <script>
-              // 2. This code loads the IFrame Player API code asynchronously.
-              var tag = document.createElement('script');
+        $myApiKey = get_field('api_key', 'option');
+        $myChannelID = get_field('channel_id', 'option');
+        $maxResults="6";
 
-              tag.src = "https://www.youtube.com/iframe_api";
-              var firstScriptTag = document.getElementsByTagName('script')[0];
-              firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        $myQuery = "https://www.googleapis.com/youtube/v3/search?key=$myApiKey&channelId=$myChannelID&part=snippet,id&order=date&maxResults=$maxResults";
+        $videoList = file_get_contents($myQuery);
 
-              // 3. This function creates an <iframe> (and YouTube player)
-              //    after the API code downloads.
-              var player_<?php echo $i; ?>;
-              var containerWidth_<?php echo $i; ?> = $("#player_<?php echo $i; ?>").parent().innerWidth();
-              var videoHeight_<?php echo $i; ?> = (360/640) * containerWidth_<?php echo $i; ?>;
-
-              // 4. The API will call this function when the video player is ready.
-              function onPlayerReady(event) {
-                //event.target.playVideo();
-              }
-
-              // 5. The API calls this function when the player's state changes.
-              //    The function indicates that when playing a video (state=1),
-              //    the player should play for six seconds and then stop.
-              var done = false;
-              function onPlayerStateChange(event) {
-                if (event.data == YT.PlayerState.PLAYING && !done) {
-                  setTimeout(stopVideo, 6000);
-                  done = true;
-                }
-              }
-              function stopVideo() {
-                player.stopVideo();
-              }
-            </script>
-          </div>
-          <h3 class="title">Exemplo 1: a responsabilidade das instituições de ensino</h3>
+        $decoded = json_decode($videoList, true);
+        foreach ($decoded['items'] as $items) {
+          $id = $items['id']['videoId'];
+          $title= $items['snippet']['title'];
+          $description = $items['snippet']['description'];
+          $thumbnail = $items['snippet']['thumbnails']['high']['url'];
+      ?>
+        <a href="https://www.youtube.com/watch?v=<?php echo $id; ?>" class="video" target="_blank">
+            <div class="cover">
+              <div class="thumbnail">
+                <img src='<?php echo $thumbnail; ?>'>
+              </div>
+            </div>
+            <h3 class="title"><?php echo $title; ?></h3>
         </a>
-
-      <?php endfor; ?>
-
-      <script>
-        function onYouTubeIframeAPIReady() {
-          <?php for ($i = 1; $i <= 3; $i++): ?>
-            player_<?php echo $i; ?> = new YT.Player('player_<?php echo $i; ?>', {
-              height: videoHeight_<?php echo $i; ?>,
-              width: containerWidth_<?php echo $i; ?>,
-              videoId: 'wrRPEPJlH44',
-              events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-              }
-            });
-          <?php endfor; ?>
-        }
-      </script>
-
+      <?php  }; ?>
     </div>
 
     <div class="more-videos">
-      <a class="esm-button">Carregar mais</a>
+      <a class="esm-button" href="https://www.youtube.com/channel/<?php echo $myChannelID; ?>" target="_blank">Acessar canal</a>
     </div>
 
 </div>
