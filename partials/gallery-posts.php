659b35e84ea028f7fc7cc650f688b8508d487f82
Caê Penna <caepenna@gmail.com>
2019-06-04 23:53:47 -0300

    Fix archive thumbnails

--- partials/gallery-posts.php
+++ partials/gallery-posts.php
@@ -32,48 +32,46 @@
 ?>
 
 <section class="section-postlist">
-    <div class="list">
-        <div id="ajaxContainer">
-            <?php
-                $i = 1;
-                while ($posts_query -> have_posts()) : $posts_query -> the_post();
-                $thumbnail = get_post_thumbnail_id();
-                $titulo = get_the_title();
-                $link = get_permalink();
-                $postcat = get_the_category( $post->ID );
-                if ( ! empty( $postcat ) ) {
-                    $area_slug = $postcat[0]->slug;
-                    $area_name = $postcat[0]->name;
+  <div class="list">
+    <div id="ajaxContainer">
+      <?php
+        $i = 1;
+        while ($posts_query -> have_posts()) : $posts_query -> the_post();
+        $thumbnail = get_post_thumbnail_id();
+        $titulo = get_the_title();
+        $link = get_permalink();
+        $postcat = get_the_category( $post->ID );
+        if ( ! empty( $postcat ) ) {
+          $area_slug = $postcat[0]->slug;
+          $area_name = $postcat[0]->name;
 
-                    $term_id = $postcat[0]->term_id;
-                    $taxonomy = $postcat[0]->taxonomy;
+          $term_id = $postcat[0]->term_id;
+          $taxonomy = $postcat[0]->taxonomy;
 
-                }
-            ?>
-            <a href="<?php echo $link; ?>" class="post">
-                <figure class="thumb">
-                  <?php echo wp_get_attachment_image($thumbnail, 'large'); ?>
-                  <h4 class="tag"><?php echo $area_name; ?></h4>
-                </figure>
-                <div>
-                  <h3 class="title"><?php echo $titulo; ?></h3>
-                </div>
-              </a>
+        }
+      ?>
+      <a href="<?php echo $link; ?>" class="post">
+        <figure class="thumb">
+          <?php echo wp_get_attachment_image($thumbnail, 'small'); ?>
+          <h4 class="tag"><?php echo $area_name; ?></h4>
+        </figure>
+        <h3 class="title"><?php echo $titulo; ?></h3>
+      </a>
 
-            <?php $i++; endwhile; ?>
-        </div>
-          <!-- A seguir, tem essas classes "d-none" e "d-inline-block" do bootstrap que eu uso pra esconder
-          e mostrar esse botão com display:none. Mas você pode usar a que quiser. esse id também vai ser necessário. -->
-          <a
-            id="more_posts"
-            class="esm-button loadmore <?php if ($i <= $posts_count): ?>d-inline-block<?php else: ?>d-none<?php endif; ?>"
-            data-order="<?php echo $order; ?>"
-            data-post-type="<?php echo $postType; ?>"
-            data-ppp="<?php echo $postsPerPage; ?>"
-            data-offset="<?php echo $postsPerPage; ?>">
-            Ver mais
-          </a>
+      <?php $i++; endwhile; ?>
     </div>
+    <!-- A seguir, tem essas classes "d-none" e "d-inline-block" do bootstrap que eu uso pra esconder
+    e mostrar esse botão com display:none. Mas você pode usar a que quiser. esse id também vai ser necessário. -->
+    <a
+      id="more_posts"
+      class="esm-button loadmore <?php if ($i <= $posts_count): ?>d-inline-block<?php else: ?>d-none<?php endif; ?>"
+      data-order="<?php echo $order; ?>"
+      data-post-type="<?php echo $postType; ?>"
+      data-ppp="<?php echo $postsPerPage; ?>"
+      data-offset="<?php echo $postsPerPage; ?>">
+      Ver mais
+    </a>
+  </div>
 </section>
 
 <?php endif; ?>
