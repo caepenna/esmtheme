f4ad9906a0f5daf1c63019af49e39b00b24d7e37
Vítor Marques de Faria <vtrmrx@gmail.com>
2019-06-06 21:32:48 -0300

    cria função slugify

--- functions.php
+++ functions.php
@@ -54,6 +54,25 @@ function custom_editor_styles() {
 }
 add_action( 'admin_init', 'custom_editor_styles' );
 
+function slugify($text) {
+  // replace non letter or digits by -
+  $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+  // transliterate
+  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+  // remove unwanted characters
+  $text = preg_replace('~[^-\w]+~', '', $text);
+  // trim
+  $text = trim($text, '-');
+  // remove duplicate -
+  $text = preg_replace('~-+~', '-', $text);
+  // lowercase
+  $text = strtolower($text);
+  if (empty($text)) {
+    return '';
+  }
+  return $text;
+}
+
 
 // Custom post types
 
