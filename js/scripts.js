c09110bcd426a0832dfeba5e1e5a79d6faa5ce4b
Caê Penna <caepenna@gmail.com>
2019-08-05 20:26:45 -0300

    Fix scrolling bug on menu

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
   $('.js-menu').on('click', function(){
     $(this).toggleClass('--x');
     $('.esm-header').toggleClass('-open');
+    $('body').toggleClass('-overflowhidden');
   });
 });
